fdc9c54cc073646d8221190daa5cddfcc149d0d9
PrashanthaTP <tpprashantha@gmail.com>
2022-02-26 16:55:43 +0530

    Fix(formatter): fix escape codes

--- plogger/main.py
+++ plogger/main.py
@@ -5,84 +5,112 @@ from os import path as os_path
 logging.basicConfig(format="[%(levelname)s]  %(name)s | %(lineno)s : %(msg)s")
 
 
-CURR_DIR = os_path.dirname(os_path.abspath(__file__))
-SETTINGS_JSON = os_path.join(CURR_DIR, "log_settings.json")
+curr_dir = os_path.dirname(os_path.abspath(__file__))
+settings_json = os_path.join(curr_dir, "log_settings.json")
 
-Colors = namedtuple("Colors", ["FG", "BG", "STYLE"])
-LogLevels = namedtuple("LogLevels", ["DEBUG", "INFO", "WARNING", "ERROR"])
-LEVELS = LogLevels(logging.DEBUG, logging.INFO,
-                   logging.INFO, logging.ERROR)
+colors = namedtuple("colors", ["fg", "bg", "style"])
+loglevels = namedtuple("loglevels", ["debug", "info", "warning", "error"])
+levels = loglevels(logging.debug, logging.info,
+                   logging.info, logging.error)
 
 
 class CustomFormatter(logging.Formatter):
-    black = Colors(30, 40, "")
-    red = Colors(31, 41, "")
-    green = Colors(32, 42, "")
-    yellow = Colors(33, 43, "")
-    blue = Colors(34, 44, "")
-    magenta = Colors(35, 45, "")
-    cyan = Colors(36, 46, "")
-    white = Colors(37, 47, "")
+    """
+    references : 
+        - https://www.ing.iac.es//~docs/external/bash/abs-guide/colorizing.html#:~:text=the%20simplest%2c%20and%20perhaps%20most,%5b0%22%20switches%20it%20off.
+        - https://stackoverflow.com/questions/287871/how-to-print-colored-text-to-the-terminal
+    """
+    black = colors(30, 40, "")
+    red = colors(31, 41, "")
+    green = colors(32, 42, "")
+    yellow = colors(33, 43, "")
+    blue = colors(34, 44, "")
+    magenta = colors(35, 45, "")
+    cyan = colors(36, 46, "")
+    white = colors(37, 47, "")
     reset = "0"
     bold = "1"
+    default_fmt = "[%(levelname)s]  %(name)s | %(lineno)s : %(msg)s"
+    default_fmts = loglevels(default_fmt, default_fmt,
+                             default_fmt, default_fmt)
+    default_level_styles = loglevels(
+        colors(green.fg, "", ""),
+        colors(cyan.fg, "", ""),
+        colors(red.fg, "", ""),
+        colors(red.fg, "", ""),
+    )
 
     def escape(self, code):
         return f"\033[{code}m"
 
-    def full_color_code(self, fmt, colors: Colors):
-        fg, bg, style = colors.FG, colors.BG, colors.STYLE
-        first_chunk = self.escape(f"{style};{fg};{bg}")
-        last_chunk = self.escape(CustomFormatter.escape)
+    def full_color_code(self, fmt, color: colors):
+        fg, bg, style = color.fg, color.bg, color.style
+        if fg != "":
+            fg = f"{fg};"
+        else:
+            fg = f"{CustomFormatter.white};"
+        if bg == "":
+            fg = fg.strip(";")
+        if style != "":
+            style = f"{style};"
+
+        first_chunk = self.escape(f"{style}{fg}{bg}")
+        last_chunk = self.escape(CustomFormatter.reset)
         return f"{first_chunk}{fmt}{last_chunk}"
 
-    def __init__(self, fmts: LogLevels, level_styles: LogLevels):
-        self.settings = level_styles
+    def __init__(self, fmts: loglevels = None, level_styles: loglevels = None):
+        self.settings = level_styles or CustomFormatter.default_level_styles
+        fmts = fmts or CustomFormatter.default_fmts
         self.formatters = {
             logging.DEBUG: logging.Formatter(self.full_color_code(
-                fmts.DEBUG, self.settings.DEBUG)),
+                fmts.debug, self.settings.debug)),
             logging.INFO: logging.Formatter(self.full_color_code(
-                fmts.INFO, self.settings.INFO)),
+                fmts.info, self.settings.info)),
             logging.WARNING: logging.Formatter(self.full_color_code(
-                fmts.WARNING, self.settings.WARNING)),
+                fmts.warning, self.settings.warning)),
             logging.ERROR: logging.Formatter(self.full_color_code(
-                fmts.ERROR, self.settings.ERROR))
+                fmts.error, self.settings.error))
         }
 
     def format(self, record):
-        return self.formatters.get(record.levelno, logging.DEBUG)
+        return self.formatters.get(record.levelno, logging.Formatter(CustomFormatter.default_fmt)).format(record)
 
 
 def read_settings():
     import json
     settings = {}
-    with open(SETTINGS_JSON, 'r') as file:
+    with open(settings_json, 'r') as file:
         settings = json.load(file)
     return settings
 
 
-def __get_logger(name: str, level=logging.DEBUG) -> logging.Logger:
+def __get_logger(name: str, level=logging.debug) -> logging.Logger:
     fmt = "[%(levelname)s] %(name)s | %(lineno)s : %(msg)s"
     logger = logging.getLogger(name)
-    if logger.hasHandlers():
+
+    if logger.hasHandlers() and len(logger.handlers) > 0:
         # then logger was already created with handlers
         # so we return the same instance of logger
+        print(logger.handlers)
+        logger.warning("%(name)s logger already exists!" % {"name": name})
         return logger
     logger.setLevel(level)
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(fmt, "%b %d,%Y %H:%M:%S"))
+    #handler.setFormatter(logging.Formatter(fmt, "%b %d,%Y %H:%M:%S"))
+    handler.setFormatter(CustomFormatter())
     logger.addHandler(handler)
     logger.propagate = False
     return logger
 
 
-logger = None
+mlogger = None
 
 
 def init():
-    global logger
-    if logger is None:
-        logger = __get_logger(f"plogger_{os_path.basename(__file__)}",
-                              logging.DEBUG)
+    global mlogger
+    if mlogger is None:
+        mlogger = __get_logger(f"plogger_{os_path.basename(__file__)}",
+                               logging.DEBUG)
 
 
 def require_init(fun):
@@ -93,21 +121,30 @@ def require_init(fun):
 
 
 @require_init
+def get_global_logger():
+    global mlogger
+    return mlogger
+
+
+@require_init
 def set_verbosity(level):
-    global logger
-    logger.setLevel(level)
+    global mlogger
+    mlogger.setLevel(level)
 
 
 @require_init
 def get_logger(name: str = "", level=logging.DEBUG) -> logging.Logger:
-    global logger
+    global mlogger
     if len(name) == 0:
-        logger.error("name cannot be empty")
-    mLogger = __get_logger(name, level)
-    return mLogger
+        mlogger.error("name cannot be empty")
+    new_logger = __get_logger(name, level)
+    return new_logger
 
 
 if __name__ == "__main__":
-    logger1 = get_logger("test", logging.ERROR)
+    print("Main........")
+    CustomFormatter()
+    logger1 = get_logger("aytala", logging.DEBUG)
     logger1.info("logger1")
-    logger2 = get_logger("")
+    logger2 = get_logger("test2")
+    logger2.debug('print')
